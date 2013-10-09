0220e4770b8ea760debb79a43d5ff4abe6ea9e0e
jden <jason@denizac.org>
2013-10-09 11:28:44 -0700

    Initial Commit

---
+++ ADURL/ADURL.h
@@ -0,0 +1,13 @@
+//
+//  ADURL.h
+//  ADURL
+//
+//  Created by Jason Denizac on 10/9/13.
+//  Copyright (c) 2013 AgileMD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ADURL : NSObject
+
+@end
---
+++ ADURL/ADURL.m
@@ -0,0 +1,13 @@
+//
+//  ADURL.m
+//  ADURL
+//
+//  Created by Jason Denizac on 10/9/13.
+//  Copyright (c) 2013 AgileMD. All rights reserved.
+//
+
+#import "ADURL.h"
+
+@implementation ADURL
+
+@end
